dc69a8f39eb9f4852471827f2b5106bf43cbf0e7
sdrummeyjr <33046696+sdrummeyjr@users.noreply.github.com>
2019-07-02 22:14:06 -0400

    Started AST and Parser

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Ramble programming language (goRamble implementation!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Ramble programming language (goRamble implementation)!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
